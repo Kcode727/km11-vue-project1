70e7259f786e9a13a6e75272d7f4f0c33bf06ba1
Kcode727 <meshramkp@rknec.edu>
2024-10-26 14:30:18 +0530

    Todo list Logic

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <script setup>
-import {ref} from "vue";
-const counter = ref(0); 
- 
-const increment =() => {
-  counter.value++;
-}
-const decrement =() => {
-  counter.value--;
-}
+import { ref } from "vue";
+
+// Mock TODO data
+const todos = ref([
+  { description: "First todo", dueDate: "2024-10-30", completed: false },
+  { description: "Second todo", dueDate: "2024-11-05", completed: true },
+  { description: "Third todo", dueDate: "2024-12-01", completed: false },
+]);
 </script>
 
 <template>
   <header>
-
+    <nav>
+      <ul>
+        <li><strong>Todo Application</strong></li>
+      </ul>
+      <ul>
+        <li><a href="/todos">List Todos</a></li>
+        <li><a href="/todos/create">Add Todo</a></li>
+      </ul>
+    </nav>
   </header>
 
-  <main>
-   <h1>Counter: {{ counter }}</h1>
-   <button @click="increment">Increment</button>
-   <button v-on:click="decrement">Decrement</button>
+  <main class="container">
+    <h1>TODO List</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Due Date</th>
+          <th>Completed?</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(todo, index) in todos" :key="index">
+          <td>{{ todo.description }}</td>
+          <td>{{ todo.dueDate }}</td>
+          <td v-if="todo.completed">Yes</td>
+          <td v-else>No</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 
 <style scoped>
-
 </style>
