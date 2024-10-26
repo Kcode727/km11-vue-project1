25ef1270dc460e841abf9f05929d17cca8739c55
Kcode727 <meshramkp@rknec.edu>
2024-10-26 14:03:25 +0530

    Counter Logic

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from "vue";
+const counter = ref(0); 
+ 
+const increment =() => {
+  counter.value++;
+}
+const decrement =() => {
+  counter.value--;
+}
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+   <h1>Counter: {{ counter }}</h1>
+   <button @click="increment">Increment</button>
+   <button v-on:click="decrement">Decrement</button>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
